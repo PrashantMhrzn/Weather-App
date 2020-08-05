2d7755f4506b364dae45f87fedc0e6445dc334d3
PrashantMhrzn <prashantmaharjan5@gmail.com>
2020-08-05 16:22:09 +0545

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Weather-App
+# Weather App
 A simple program that shows the weather using OpenWeatherMap One Call API.
+*Note: Inorder to use the One Call API provided by OpenWeatherMap you need to get the api key from [here](https://openweathermap.org/price) and add the required information.*
+## Installation
+
+```bash
+pip/pip3 install -r requirements.txt
+```
+## Usage
+
+```bash
+python/python3 main.py
+
+```
+## License
+[MIT](https://github.com/PrashantMhrzn/Weather-App/blob/master/LICENSE)
---
+++ main.py
@@ -0,0 +1,30 @@
+try:
+    import requests
+    import json
+except ModuleNotFoundError:
+    print('Modules not found. Please run pip install -r requirements.txt to install the required modules.')
+    exit()
+
+
+def weather_info(data):
+    max_temp = data['daily'][1]['temp']['max']
+    min_temp = data['daily'][1]['temp']['min']
+    current_temp = data['current']['temp']
+    weather = data['daily'][1]['weather'][0]['description']
+    print(f'maximum temprature: {max_temp} C')
+    print(f'minimum temprature: {min_temp} C')
+    print(f'current temprature: {current_temp} C')
+    print(f'today\'s condition: {weather}')
+
+
+def api_data():
+    # goto https://openweathermap.org/api/one-call-api for reference
+    url = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={YOUR API KEY}'
+    res = requests.get(url)
+    data = res.text
+    parsed = dict(json.loads(data))
+    return parsed
+
+
+print('Today\'s weather forecast:\n')
+weather_info(api_data())
